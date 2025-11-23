bb450ff46e808209e74652049e7fa8cf0ca5ed1f
Asif Alam <104073769+Looperrrrrr@users.noreply.github.com>
2025-11-23 13:13:11 +0000

    done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,29 +1,20 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
 
     const username = "looper";
 
     useEffect(() => {
-        
-        fetch(`https://playground.4geeks.com/todo/users/${username}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(resp => {
-                console.log("User created.", resp.ok);
-                console.log("Status: ", resp.status);
 
-            })
-            .catch(error => console.log("Error creating user", error));
+
 
         // Trae lista de tareas de la API
         bringTodo()
@@ -32,85 +23,97 @@ const Home = () => {
 
     // Metodo GET para traer lista de tareas.
     const bringTodo = async () => {
-        return fetch(`https://playground.4geeks.com/todo/users/${username}`)
-            .then(resp => {
-                console.log("Traer tareas: ", resp.ok);
-                console.log("Status: ", resp.status)
-                return resp.json();
-            })
-            .then(data => {
-                console.log("Tareas cargadas: ", data.todos)
-                setTodos(data.todos);
-            })
-            .catch(error => console.log("Error al cargar tareas: ", error));
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/users/looper", requestOptions);
+            const result = await response.json();
+            setTodos(result.todos)
+        } catch (error) {
+            console.error(error);
+        };
     };
 
     // Metodo POST para Agrega tarea.
     const addTask = async () => {
-        await fetch("https://playground.4geeks.com/todo/todos/moisesfspt120", {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "label": inputValue,
+            "is_done": false
+        });
+
+        const requestOptions = {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                label: task
-            })
-        })
-            .then(() => {
-                traerTodo()
-            }).then(resp => {
-                console.log("tarea agregada: ", resp.ok);
-                console.log("Status: ", resp.status);
-                return resp.json();
-            })
-            .then(data => {
-                setTodos(data.todos);
-                setTask("");
-            })
-            .catch(console.log("Error al agregar tarea:"));
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/todos/looper", requestOptions);
+            const result = await response.json();
+            console.log(result)
+            bringTodo()
+        } catch (error) {
+            console.error(error);
+        };
     }
 
     // Metodo DELETE para eliminar tarea.
     const removeTask = async (id) => {
-        await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
-            method: "DELETE"
-        })
-            .then(resp => {
-                console.log(`Eliminar tarea ${id}:`, resp.ok)
-                console.log("Status:", resp.status);
-                return resp.json();
-            })
-            .catch(error => console.error(`Error al eliminar tarea ${id}:`, error));
+        const requestOptions = {
+            method: "DELETE",
+            redirect: "follow"
+        };
+        id=id.toString()
+        console.log(id);
+        
+        try {
+            const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, requestOptions);
+            const result = await response.json();
+            console.log(id)
+            bringTodo()
+        } catch (error) {
+            console.error(error);
+           
+        };
+
     };
 
-	return (
-		<div className="container">
-			<h1>TODO LIST</h1>
-			<ul>
-				<li><input type="text"
-					onChange={(e) => setInputValue(e.target.value)}
-					value={inputValue}
-					onKeyDown={(e) => {
-						if (e.key === "Enter" && task.trim() !== "") {
-							setTodos(todos.concat([inputValue]));
-							setInputValue("");
+    return (
+        <div className="container">
+            <h1>TODO LIST</h1>
+            <ul>
+                <li><input type="text"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && inputValue.trim() !== "") {
+                            setTodos(todos.concat([inputValue]));
                             addTask()
-						}
+                            setInputValue("");
 
-					}}
-					placeholder="What needs to be done?" /></li>
+                        }
 
-				{todos.map((item, index) => (
-					<li className="todos">
-						{item} <i class="fa-solid fa-trash trashButton" onClick={async () => {
-                await removeTask(item.id);  
-                bringTodo()}}></i>
-					</li>
-				))}
-			</ul>
-			<div>{todos.length} tasks</div>
-		</div>
-	);
+                    }}
+                    placeholder="What needs to be done?" /></li>
+
+                {todos.map((item, index) => (
+                    <li key={index} className="todos">
+                        {item.label} <i className="fa-solid fa-trash trashButton" onClick={ () => {removeTask(item.id);
+
+                        }}></i>
+                    </li>
+                ))}
+            </ul>
+            <div>{todos.length} tasks</div>
+        </div>
+    );
 };
 
 
